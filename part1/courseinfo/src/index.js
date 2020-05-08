aeac5efb45feb1b32a57e946bf02da9c31831b42
Bryan Gallardo <6342503+bryg@users.noreply.github.com>
2020-05-07 20:13:28 -0700

    index changes

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,73 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const Header = (props) => {
-  return <h1>{props.course}</h1>
-}
-
-const Part = (props) => {
-  console.log(props.part)
-  return (
-      <p>
-        {props.part.name} {props.part.exercises}
-      </p>
-  )
-}
-
-const Content = (props) => {
-  const part0 = props.parts[0];
-  const part1 = props.parts[1];
-  const part2 = props.parts[2];
-
-  return (
-    <div>
-    <Part part={part0} />
-    <Part part={part1} />
-    <Part part={part2} />
-    </div>
-  )
-}
-
-const Total = (props) => {
-  const total = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises;
-  return (
-    <div>
-      <p>Number of exercises {total}</p>
-    </div>
-  )
-}
-
-
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts}/>
+      code here
     </div>
   )
 }
 
-ReactDOM.render(
-  <App />,
+ReactDOM.render(<App />, 
   document.getElementById('root')
-);
+)
